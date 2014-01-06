1eaf029461b90f125587899f45e43f2a7ae52e0c
Jeremy Lynch <jl@adminempire.com>
2014-01-05 17:16:46 -0800

    Adding script to look for directories not owned by pacman

---
+++ scripts/pacman-disowned-directories.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+#
+# Pacman disowned dirs
+#
+#
+comm -23 <(sudo find / \( -path '/dev' -o -path '/sys' -o -path '/run' -o -path '/tmp' -o -path '/mnt' -o -path '/srv' -o -path '/proc' -o -path '/boot' -o -path '/home' -o -path '/root' -o -path '/media' -o -path '/var/lib/pacman' -o -path '/var/cache/pacman' \) -prune -o -type d -print | sed 's/\([^/]\)$/\1\//' | sort -u) <(pacman -Qlq | sort -u)
