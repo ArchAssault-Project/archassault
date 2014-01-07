50e30fa6e8d84785f49792c69d603bc0015656ae
Jeremy Lynch <jl@archassault.org>
2014-01-07 06:35:49 +0000

    gem-clearing-last-resort.sh edited online with Bitbucket
    fixing typo

--- scripts/gem-clearing-last-resort.sh
+++ scripts/gem-clearing-last-resort.sh
@@ -5,4 +5,4 @@
 # Last resort for clearing gems out that were not installed 100% properly yet
 #
 for i in `gem list --no-versions`; do gem uninstall -aIx $i; done
-or i in `gem-1.9 list --no-versions`; do gem-1.9 uninstall -aIx $i; done
+for i in `gem-1.9 list --no-versions`; do gem-1.9 uninstall -aIx $i; done
