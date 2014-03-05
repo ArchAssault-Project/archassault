ea750d1fc7e10b2d2958e17f80f1470a41936569
Jeremy Lynch <jl@adminempire.com>
2014-03-05 14:23:19 -0800

    Updating script with missing flag for shred and tweaking a little more.

--- scripts/prep-ec2-ami-for-public-release.sh
+++ scripts/prep-ec2-ami-for-public-release.sh
@@ -2,14 +2,15 @@
 #
 # Clean up an AMI to get ready for Public release
 #
-#
 # list auth keys
-# find / -name "authorized_keys" -print -exec cat {} \;
+find / -name "authorized_keys" -print -exec cat {} \;
 # list users
-# cat /etc/passwd /etc/shadow | grep -E '^[^:]*:[^:]{3,}' | cut -d: -f1
-
-
-find /root/.*history /home/*/.*history -exec shred -zn70  {} \;
-find / -name "authorized_keys" -exec shred -zn70  {} \;
-find /root/ /home/*/ -name .cvspass -exec shred -zn70  {} \;
-find /root/.subversion/auth/svn.simple/ /home/*/.subversion/auth/svn.simple/ -exec shred -zn70 {} \;
+cat /etc/passwd /etc/shadow | grep -E '^[^:]*:[^:]{3,}' | cut -d: -f1
+# Start the cleaning of things
+history -c 
+find /root/.*history /home/*/.*history -exec shred -zn70 -u {} \;
+find / -name "authorized_keys" -exec shred -zn70 -u {} \;
+find /root/ /home/*/ -name .cvspass -exec shred -zn70 -u {} \;
+find /root/.subversion/auth/svn.simple/ /home/*/.subversion/auth/svn.simple/ -exec shred -zn70 -u {} \;
+# list auth keys after clean up
+find / -name "authorized_keys" -print -exec cat {} \;
