0a38f2adca73879173fcbf8ef5c25a658c907b5a
Jeremy Lynch <jl@adminempire.com>
2014-03-05 14:45:21 -0800

    Removing ssh host keys so they are regenerated on boot. Helps prevent possible MITM attack

--- scripts/prep-ec2-ami-for-public-release.sh
+++ scripts/prep-ec2-ami-for-public-release.sh
@@ -7,10 +7,16 @@ find / -name "authorized_keys" -print -exec cat {} \;
 # list users
 cat /etc/passwd /etc/shadow | grep -E '^[^:]*:[^:]{3,}' | cut -d: -f1
 # Start the cleaning of things
+# Clean up all history
 history -c 
 find /root/.*history /home/*/.*history -exec shred -zn70 -u {} \;
+# Clean up all auth key files
 find / -name "authorized_keys" -exec shred -zn70 -u {} \;
+# Clean up any cvspass files
 find /root/ /home/*/ -name .cvspass -exec shred -zn70 -u {} \;
+# Clean up any subversion auth stuff
 find /root/.subversion/auth/svn.simple/ /home/*/.subversion/auth/svn.simple/ -exec shred -zn70 -u {} \;
+# Force regeneration of new unique ssh host key pairs.
+find /etc/ssh -type f -name "ssh_host_*" -exec shred -zn70 -u {} \;
 # list auth keys after clean up
 find / -name "authorized_keys" -print -exec cat {} \;
