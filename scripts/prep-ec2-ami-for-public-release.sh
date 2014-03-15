f3fb8a7b7d8d52964095ebb8e5a654bfb254b039
Jeremy Lynch <jl@adminempire.com>
2014-03-14 17:08:38 -0700

    Updating ec2 script

--- scripts/prep-ec2-ami-for-public-release.sh
+++ scripts/prep-ec2-ami-for-public-release.sh
@@ -7,6 +7,9 @@ find / -name "authorized_keys" -print -exec cat {} \;
 # list users
 cat /etc/passwd /etc/shadow | grep -E '^[^:]*:[^:]{3,}' | cut -d: -f1
 # Start the cleaning of things
+echo "Starting the cleaning of things. "
+# clean pacman package cache
+rm -f /var/cache/pacman/pkg/*
 # Clean up all history
 history -c 
 find /root/.*history /home/*/.*history -exec shred -zn70 -u {} \;
@@ -20,3 +23,14 @@ find /root/.subversion/auth/svn.simple/ /home/*/.subversion/auth/svn.simple/ -ex
 find /etc/ssh -type f -name "ssh_host_*" -exec shred -zn70 -u {} \;
 # list auth keys after clean up
 find / -name "authorized_keys" -print -exec cat {} \;
+# Clean up lastlog stuff
+>/var/log/wtmp
+>/var/log/btmp
+# clean up some other logs
+rm -rf /var/log/air/ /var/log/auth.log /var/log/cloud-init.log /var/log/couchdb/ /var/log/crond.log /var/log/daemon.log /var/log/dashcam.log /var/log/errors.log /var/log/everything.log /var/log/inetsim/ /var/log/kernel.log /var/log/lastlog /var/log/messages.log /var/log/old/ /var/log/openvas/ /var/log/pacman.log /var/log/radius/ /var/log/samba/ /var/log/snort/ /var/log/syslog.log /var/log/tiger/ /var/log/user.log
+# finished
+echo "We should be clean now verify"
+# list auth keys
+find / -name "authorized_keys" -print -exec cat {} \;
+# list users
+cat /etc/passwd /etc/shadow | grep -E '^[^:]*:[^:]{3,}' | cut -d: -f1
